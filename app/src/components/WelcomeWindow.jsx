11e29ca378a54f929301415be9418f2a801463f5
BernatBC <bernat.borrasc@gmail.com>
2026-01-04 10:45:07 +0100

    Fix lint

--- app/src/components/WelcomeWindow.jsx
+++ app/src/components/WelcomeWindow.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTheme } from "../contexts/theme-core";
 import { useNumberFormat } from "../contexts/number-format";
 import { CURRENCIES } from "../utils/currencies";
